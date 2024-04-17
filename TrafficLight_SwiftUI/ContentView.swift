70106c5e8d8d937a7971222d15e0480cb7e8f158
KDaria <dkdva@mail.ru>
2024-04-17 15:57:13 +0200

    Initial Commit

---
+++ TrafficLight_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Дарья Кобелева on 17.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrafficLight_SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Дарья Кобелева on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
